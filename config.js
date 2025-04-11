40986ed1cbfb9c2b24baac264b40c2fd1d067b11
Gerald Hoo <gerald.hoo@ncs.com.sg>
2025-04-11 18:40:54 +0800

    seed from github action instead

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
 const { Sequelize } = require('sequelize')
 const AWS = require('aws-sdk')
-const Product = require('./models/products')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
 })
-
-sequelize.models = {
-  Product
-}
 
 const configureAWS = () => {
   AWS.config.update({
