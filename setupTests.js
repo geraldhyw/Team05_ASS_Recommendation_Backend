1d977659c3a693004e6d0421ca226343bf37ab23
Gerald Hoo <gerald.hoo@ncs.com.sg>
2025-04-11 17:58:19 +0800

    try localhost instead

--- setupTests.js
+++ setupTests.js
@@ -47,8 +47,6 @@ const initDynamoDb = async () => {
 beforeAll(async () => {
     await sequelize.sync({ force: true }).then(async () => {
         await seedData()
-    }).catch(() => {
-        console.error(process.env.DB_HOST)
     })
     
     await initDynamoDb()
